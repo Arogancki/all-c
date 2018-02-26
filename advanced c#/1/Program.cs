a303716936c39b4bc05caad33462ea9653449339
arogancki <ziembaart@gmail.com>
2018-02-26 21:29:43 +0100

    lab1 pro c#

---
+++ advanced c#/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using ConsoleApplication1;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string fp = @"./TextFile1.txt";
+            string file = File.ReadAllText(fp);
+            HtmlTable t = new HtmlTable(file);
+            string x = t.getHTML();
+            File.WriteAllText(fp+".html", x);
+        }
+    }
+}

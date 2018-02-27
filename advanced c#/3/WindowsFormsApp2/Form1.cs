0bb2fbb2aac53bcbb50bd5df92e1f356081b5c4c
Artur Ziemba <artur.ziemba@avid.com>
2018-02-27 20:31:48 +0100

    lab 3 init

---
+++ advanced c#/3/WindowsFormsApp2/Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private OpenFileDialog openFileDialog1;
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+
+            using (var fbd = new FolderBrowserDialog())
+            {
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+
+                    string[] files = Directory.GetFiles(fbd.SelectedPath, "*.sln");
+                    if (files == null) {
+                        MessageBox.Show("No .sln file found");
+                        return;
+                    }
+
+                    string File = files[0];
+
+
+
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
